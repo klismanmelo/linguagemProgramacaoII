ebdc08e6ebeeff6bd38b0ac50c45ef99f996c882
klismanmelo <61260271+klismanmelo@users.noreply.github.com>
2021-08-24 20:19:48 -0300

    Execicio Lista 09

---
+++ src/Pacote01/Continente.java
@@ -0,0 +1,105 @@
+package Pacote01;
+
+import Pacote01.Pais;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Continente {
+    String nomeContinente;
+    ArrayList<Pais> paisContinente = new ArrayList<Pais>();
+    Pais pais;
+    Scanner leia = new Scanner(System.in);
+
+    public Continente(String nome) {
+        this.nomeContinente = nome;
+    }
+
+
+    public void setPaisContinente(){
+        paisContinente.add(pais);
+    }
+
+    public void addPais(Pais p){
+        this.paisContinente.add(p);
+    }
+
+    public float dimensaoContinente(){
+        float soma = 0;
+        for(Pais p: this.paisContinente){
+            soma = (float )(soma + p.getDimensao());
+        }
+        return soma;
+    }
+
+    public float popiulcaoContinente(){
+        float soma = 0;
+        for(Pais p: this.paisContinente){
+            soma = (float )(soma + p.getPop());
+        }
+        return soma;
+    }
+
+    public float densPop(){
+        return (float) this.popiulcaoContinente() / this.dimensaoContinente();
+    }
+
+    public Pais maiorPop(){
+        Pais paisMaiorPop = this.paisContinente.get(0);
+
+        for(int i=0; i<this.paisContinente.size(); i++){
+            Pais p = this.paisContinente.get(i);
+            if(p.getPop() > paisMaiorPop.getPop()){
+                paisMaiorPop = p;
+            }
+        }
+        return paisMaiorPop;
+    }
+
+    public Pais maenorPop(){
+        Pais paisMenorPop = this.paisContinente.get(0);
+
+        for(int i=0; i<this.paisContinente.size(); i++){
+            Pais p = this.paisContinente.get(i);
+            if(p.getPop() < paisMenorPop.getPop()){
+                paisMenorPop = p;
+            }
+        }
+        return paisMenorPop;
+    }
+
+    public Pais maiorDimensao(){
+        Pais paisMaiorDim = this.paisContinente.get(0);
+
+        for(int i=0; i<this.paisContinente.size(); i++){
+            Pais p = this.paisContinente.get(i);
+            if(p.getDimensao() > paisMaiorDim.getDimensao()){
+                paisMaiorDim = p;
+            }
+        }
+        return paisMaiorDim;
+    }
+
+    public Pais menorDimensao(){
+        Pais paisMenorDimensao = this.paisContinente.get(0);
+
+        for(int i=0; i<this.paisContinente.size(); i++){
+            Pais p = this.paisContinente.get(i);
+            if(p.getDimensao() < paisMenorDimensao.getDimensao()){
+                paisMenorDimensao = p;
+            }
+        }
+        return paisMenorDimensao;
+    }
+
+    public float razao(){
+        Pais paisMaior = this.maiorDimensao();
+        Pais paisMenor = this.menorDimensao();
+
+        return (float) (paisMaior.getDimensao() / paisMenor.getDimensao());
+    }
+
+
+
+
+}
---
+++ src/Pacote01/Pais.java
@@ -0,0 +1,78 @@
+package Pacote01;
+
+import java.util.ArrayList;
+
+
+public class Pais {
+    private String codigoIso;
+    private String nome;
+    private Integer pop;
+    private Double dimensao;
+    public ArrayList<Pais> paisFronteira = new ArrayList<Pais>();
+
+    public Pais(String codigoIso, String nome, Double dimensao, Integer pop) {
+        this.codigoIso = codigoIso;
+        this.nome = nome;
+        this.dimensao = dimensao;
+        this.pop = pop;
+    }
+
+    public String getCodigoIso() {
+        return codigoIso;
+    }
+
+    public void setCodigoIso(String codigoIso) {
+        this.codigoIso = codigoIso;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Integer getPop() {
+        return pop;
+    }
+
+    public void setPop(Integer pop) {
+        this.pop = pop;
+    }
+
+    public Double getDimensao() {
+        return dimensao;
+    }
+
+    public void setDimensao(Double dimensao) {
+        this.dimensao = dimensao;
+    }
+
+    public boolean ehOMesmoPais(Pais outroPais){
+        if( this.codigoIso.equals(outroPais.getCodigoIso())){
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    public boolean ehLimitrofe(Pais outroPais){
+        if( this.paisFronteira.contains(outroPais) ){
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    public ArrayList<Pais> vizinhosComuns(Pais outroPais){
+        ArrayList<Pais> comuns = new ArrayList<Pais>();
+        for(Pais p : this.paisFronteira){
+            if( outroPais.ehLimitrofe(p) ){
+                comuns.add(p);
+            }
+        }
+        return comuns;
+    }
+
+}
